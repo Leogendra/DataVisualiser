25c8734929b5cc4a2a43d19e4779207aa1be64b6
Gatien HADDAD <gatien.haddad@pradeo.com>
2025-11-17 14:49:07 +0100

    Chore: formatting

--- extract.py
+++ extract.py
@@ -5,12 +5,12 @@ import csv, json, os, re
 
 def format_text(text):
     accents = {
-        'a': ['à', 'ã', 'á', 'â'],
-        'e': ['é', 'è', 'ê', 'ë'],
-        'i': ['î', 'ï'],
-        'u': ['ù', 'ü', 'û'],
-        'o': ['ô', 'ö'],
-        '': ['\'', '(', ')', '"']
+        "a": ["à", "ã", "á", "â"],
+        "e": ["é", "è", "ê", "ë"],
+        "i": ["î", "ï"],
+        "u": ["ù", "ü", "û"],
+        "o": ["ô", "ö"],
+        "": ["'", "(", ")", '"']
     }
     text = str(text).strip().lower()
     for (char, special_chars) in accents.items():
@@ -25,7 +25,7 @@ def check_if_files_exists():
 
     for file_path in ["data/merge_words.txt", "data/pixels_words.txt"]:
         if not(os.path.exists(file_path)):
-            with open(file_path, 'w'):
+            with open(file_path, "w"):
                 pass
 
 
@@ -42,12 +42,12 @@ def generate_sentences(splitted_word):
 
 
 def parse_sentence(raw_sentence):
-    splitted_word = raw_sentence.split(' ')
+    splitted_word = raw_sentence.split(" ")
     for i in range(len(splitted_word)):
-        splitted_word[i] = splitted_word[i].split(',')
+        splitted_word[i] = splitted_word[i].split(",")
     all_sentences = generate_sentences(splitted_word)
     for i in range(len(all_sentences)):
-        all_sentences[i] = all_sentences[i].replace('-', ' ')
+        all_sentences[i] = all_sentences[i].replace("-", " ")
     return all_sentences
 
 
@@ -94,42 +94,41 @@ def merge_data(merge_words, args):
 
 
 def parse_habits(file_path):
-    csv_content = open(file_path, 'r', encoding='utf-8').read()
+    csv_content = open(file_path, "r", encoding="utf-8").read()
     csv_lines = csv_content.strip().split("\n")
     reader = csv.DictReader(csv_lines)
 
     habits = {}
     for row in reader:
-        date = row['Date']
+        date = row["Date"]
         for key, value in row.items():
-            if key != 'Date':
-                if value == '2':
-                    if key in habits:
-                        habits[key].append(date)
+            if key != "Date":
+                if (value in ["2", "3"]):
+                    currentKey = key
+                    if (value == "3"):
+                        currentKey += "_skip"
+
+                    if (currentKey in habits):
+                        habits[currentKey].append(date)
                     else:
-                        habits[key] = [date]
-                elif value == '3':
-                    if key+'_skip' in habits:
-                        habits[key+'_skip'].append(date)
-                    else:
-                        habits[key+'_skip'] = [date]
+                        habits[currentKey] = [date]
 
     return habits
 
 
 def parse_tally(file_path):
-    csv_content = open(file_path, 'r', encoding='utf-8').read()
+    csv_content = open(file_path, "r", encoding="utf-8").read()
     csv_lines = csv_content.strip().split("\n")
     reader = csv.DictReader(csv_lines)
 
-    counter_name = ''
+    counter_name = ""
     tally = []
     for row in reader:
         counter_name = row["Counter Name"]
         if row["Count Action"] == "1":
-            timestamp = row['Timestamp']
+            timestamp = row["Timestamp"]
             # date with format "YYYY-MM-DD"
-            date = timestamp.split(' ')[0]
+            date = timestamp.split(" ")[0]
             tally.append(date)
 
     return {counter_name: tally}
@@ -159,7 +158,7 @@ class Pixel:
 
 
 def parse_pixels(file_path, words_to_extract=[]):
-    with open(file_path, 'r', encoding='utf-8') as f:
+    with open(file_path, "r", encoding="utf-8") as f:
         pixels = json.load(f)
 
     parsed_pixels = []
@@ -172,7 +171,7 @@ def parse_pixels(file_path, words_to_extract=[]):
         clean_word = format_text(word)
         for pixel in parsed_pixels:
             found = False
-            pattern = re.compile(fr'\b{re.escape(clean_word)}\b') # Whole word only
+            pattern = re.compile(fr"\b{re.escape(clean_word)}\b") # Whole word only
             # if clean_word in pixel.notes:
             if re.search(pattern, pixel.notes):
                 found = True
@@ -202,13 +201,13 @@ def clean_data(data, to_clean=[]):
 def write_parsed_data(data, file_path, write_keys=False):
     if len(data) > 0:
         # Export data to js file
-        with open(file_path, 'w', encoding='utf-8') as f:
+        with open(file_path, "w", encoding="utf-8") as f:
             f.write("var parsedData = ")
             f.write(json.dumps(data, ensure_ascii=False, indent=4))
 
         if write_keys:
-            with open("data/keys.txt", 'w', encoding='utf-8') as f:
-                f.write(', '.join(data.keys()))
+            with open("data/keys.txt", "w", encoding="utf-8") as f:
+                f.write(", ".join(data.keys()))
         print(f"Data exported to {file_path}")
     else:
         print("No data found")
@@ -237,7 +236,7 @@ def export_to_pixel_data(data, file_path):
     pixels_list = list(pixels.values())
 
     if (len(pixels_list) > 0):
-        with open(file_path, 'w', encoding='utf-8') as f:
+        with open(file_path, "w", encoding="utf-8") as f:
             f.write(json.dumps(pixels_list, ensure_ascii=False, indent=4))
         print(f"Data converted to {file_path}")
 
@@ -248,7 +247,7 @@ def auto_parse_all(dataDirectory):
 
     # Parse merge_words
     merge_words = {}
-    with open(f"{dataDirectory}/merge_words.txt", 'r', encoding='utf-8') as f:
+    with open(f"{dataDirectory}/merge_words.txt", "r", encoding="utf-8") as f:
         for line in f:
             key, values = line.strip().split(":")
             splitted_values = values.split(";")
@@ -262,7 +261,7 @@ def auto_parse_all(dataDirectory):
 
     # Parse pixels_words
     pixels_words = []
-    with open(f"{dataDirectory}/pixels_words.txt", 'r', encoding='utf-8') as f:
+    with open(f"{dataDirectory}/pixels_words.txt", "r", encoding="utf-8") as f:
         for line in f:
             parsed_phrases = parse_sentence(line.strip())
             for word in parsed_phrases:
